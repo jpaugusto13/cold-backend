a784eb1a966f4b26b68fe5ac35edcc48fc7208f7
João Pedro Augusto <103483580+Jpedroaugusto@users.noreply.github.com>
2023-06-02 05:46:50 -0300

    Fix: Erro de CORS na rota register

--- src/controller/registerController.js
+++ src/controller/registerController.js
@@ -1,5 +1,6 @@
 const db = require('../db/db');
 const Authentication = require('../auth/auth');
+const jwt = require('jsonwebtoken');
 const {
   collection,
   addDoc,
@@ -9,6 +10,7 @@ const {
 } = require('firebase/firestore');
 
 const usersRef = collection(db, 'users');
+const secretKey = 'abacate';
 
 class RegisterController {
   static async register(req, res) {
@@ -20,14 +22,15 @@ class RegisterController {
     if (!response.empty) {
       return res.status(409).json({ mensage: 'Usuário já cadastrado!' });
     }
-    const token = Authentication.geraToken({ name, email });
+    // const token = Authentication.geraToken({ name, email });
+    const token = jwt.sign({ email }, secretKey, { expiresIn: '1h' });
 
     const user = {
-      name,
-      email,
-      password,
+      name: name,
+      email: email,
+      password: password,
       money: 0,
-      token,
+      token: token,
     };
     await addDoc(usersRef, user);
     return res.status(201).json({ mensage: 'Usuário cadastrado!' });
