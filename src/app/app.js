d7a5159531eccdd7c7e444341b2d6abedad5b722
João Pedro Augusto <103483580+Jpedroaugusto@users.noreply.github.com>
2023-06-02 05:37:23 -0300

    Permitindo todas os dominios possam fazer requisição

--- src/app/app.js
+++ src/app/app.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://cold-frontend.vercel.app');
+  res.header('Access-Control-Allow-Origin', '*');
   next();
 });
 
