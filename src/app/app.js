00e62641fa168be300e58f4a95789e560754e2b7
João Pedro Augusto <103483580+Jpedroaugusto@users.noreply.github.com>
2023-06-02 05:49:41 -0300

    Adicionando novas origins

--- src/app/app.js
+++ src/app/app.js
@@ -15,7 +15,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', [
+    'http://localhost:5173/',
+    'https://cold-backend.vercel.app/',
+  ]);
   next();
 });
 
