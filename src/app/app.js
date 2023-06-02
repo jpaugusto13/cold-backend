4978cc8a0086ff52377608f1cd262d691967093c
João Pedro Augusto <103483580+Jpedroaugusto@users.noreply.github.com>
2023-06-02 05:54:11 -0300

    Removendo a ORIGIN localhost

--- src/app/app.js
+++ src/app/app.js
@@ -15,10 +15,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', [
-    'http://localhost:5173/',
-    'https://cold-backend.vercel.app/',
-  ]);
+  res.header('Access-Control-Allow-Origin', 'https://cold-backend.vercel.app/');
   next();
 });
 
