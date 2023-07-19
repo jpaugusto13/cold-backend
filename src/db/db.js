39a491fe03a231faef2b89e6797ee953c36b90da
João Pedro Augusto <103483580+Jpedroaugusto@users.noreply.github.com>
2023-07-19 08:34:48 -0300

    .

--- src/db/db.js
+++ src/db/db.js
@@ -2,11 +2,27 @@ const { initializeApp } = require('firebase/app');
 const { getFirestore } = require('firebase/firestore');
 require('dotenv').config();
 
-const firebaseApp = initializeApp({
-  apiKey: process.env.API_KEY,
-  authDomain: 'cold-db482.firebaseapp.com',
-  projectId: 'cold-db482',
-});
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyCop8EI6gTWdreNposANuhRnyXhfoALrlw',
+  authDomain: 'teste-85c3b.firebaseapp.com',
+  projectId: 'teste-85c3b',
+  storageBucket: 'teste-85c3b.appspot.com',
+  messagingSenderId: '198383591523',
+  appId: '1:198383591523:web:81234b3fc124c303591856',
+  measurementId: 'G-S7CMJVC1R5',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 const db = getFirestore(firebaseApp);
 
