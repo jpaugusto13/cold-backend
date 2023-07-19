7a1022a893c41a90b95b8572a34b395ee6ffc1c3
João Pedro Augusto <103483580+Jpedroaugusto@users.noreply.github.com>
2023-07-19 02:33:45 -0300

    .

--- src/app/app.js
+++ src/app/app.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
   res.header(
     'Access-Control-Allow-Origin',
-    'https://cold-frontend.vercel.app/'
+    'https://cold-frontend.vercel.app'
   );
   next();
 });
