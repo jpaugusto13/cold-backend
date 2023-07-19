a705cf7ace7fc0ca4ed7fbc0dc602b7c0eb59517
João Pedro Augusto <103483580+Jpedroaugusto@users.noreply.github.com>
2023-07-19 08:39:19 -0300

    .

--- src/db/db.js
+++ src/db/db.js
@@ -2,14 +2,6 @@ const { initializeApp } = require('firebase/app');
 const { getFirestore } = require('firebase/firestore');
 require('dotenv').config();
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
-import { getAnalytics } from 'firebase/analytics';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: 'AIzaSyCop8EI6gTWdreNposANuhRnyXhfoALrlw',
   authDomain: 'teste-85c3b.firebaseapp.com',
@@ -22,8 +14,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
-const db = getFirestore(firebaseApp);
+const db = getFirestore(app);
 
 module.exports = db;
